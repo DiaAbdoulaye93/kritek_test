0f1d6e69dd21f0d5e6dadd4764f8392eced9c949
Abdoulaye <diaabdoulayedjibril@gmail.com>
2022-04-11 13:37:20 +0000

    invoice form integration

--- kritek_test/src/Controller/InvoiceController.php
+++ kritek_test/src/Controller/InvoiceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Invoice;
+use App\Entity\InvoiceLine;
+use App\Form\InvoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +14,13 @@ class InvoiceController extends AbstractController
     #[Route('/invoice', name: 'app_invoice')]
     public function index(): Response
     {
+     
+        $invoice = new Invoice();
+        $invoiceLines = new InvoiceLine();
+        $invoice->getInvoiceLines()->add($invoiceLines);
+        $form = $this->createForm(InvoiceType::class, $invoice);
         return $this->render('invoice/index.html.twig', [
-            'controller_name' => 'InvoiceController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ kritek_test/src/Form/InvoiceLineType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\InvoiceLine;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FloatType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InvoiceLineType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('quantity', NumberType::class, ['attr' => ['class' => 'form-control']])
+            ->add('amount', NumberType::class, ['attr' => ['class' => 'form-control']])
+            ->add('vat_amount', NumberType::class, ['attr' => ['class' => 'form-control']])
+            ->add('total_with_vat')
+            ->add('invoice_id');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => InvoiceLine::class,
+        ]);
+    }
+}
