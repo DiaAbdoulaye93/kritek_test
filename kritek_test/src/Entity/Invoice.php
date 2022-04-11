e03561ba4700b247883951fe904c99eb3fde8bdd
Abdoulaye <diaabdoulayedjibril@gmail.com>
2022-04-11 11:20:35 +0000

    invoice entity created

---
+++ kritek_test/src/Entity/Invoice.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvoiceRepository::class)]
+class Invoice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'date')]
+    private $invoice_date;
+
+    #[ORM\Column(type: 'integer')]
+    private $invoice_number;
+
+    #[ORM\Column(type: 'integer')]
+    private $costumer_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInvoiceDate(): ?\DateTimeInterface
+    {
+        return $this->invoice_date;
+    }
+
+    public function setInvoiceDate(\DateTimeInterface $invoice_date): self
+    {
+        $this->invoice_date = $invoice_date;
+
+        return $this;
+    }
+
+    public function getInvoiceNumber(): ?int
+    {
+        return $this->invoice_number;
+    }
+
+    public function setInvoiceNumber(int $invoice_number): self
+    {
+        $this->invoice_number = $invoice_number;
+
+        return $this;
+    }
+
+    public function getCostumerId(): ?int
+    {
+        return $this->costumer_id;
+    }
+
+    public function setCostumerId(int $costumer_id): self
+    {
+        $this->costumer_id = $costumer_id;
+
+        return $this;
+    }
+}
